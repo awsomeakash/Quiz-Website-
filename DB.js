30708eacaea7c84db117982dfe1b9f6505c4a3bb
awsomeakash <40378546+awsomeakash@users.noreply.github.com>
2021-04-16 15:01:13 +0530

    Add files via upload
    Worked on question database

---
+++ DB.js
@@ -0,0 +1,36 @@
+const quizDB = [
+    {
+        question: "Q1:Smaple Queation 1?",
+        a: "option1",
+        b: "option2",
+        c: "option3",
+        d: "option4",
+        ans: "ans4"
+    },
+    {
+        question: "Q1:Smaple Queation 2?",
+        a: "option1",
+        b: "option2",
+        c: "option3",
+        d: "option4",
+        ans: "ans1"
+    },
+    {
+        question: "Q1:Smaple Queation 3?",
+        a: "option1",
+        b: "option2",
+        c: "option3",
+        d: "option4",
+        ans: "ans4"
+    },
+    {
+        question: "Q1:Smaple Queation 4?",
+        a: "option1",
+        b: "option2",
+        c: "option3",
+        d: "option4",
+        ans: "ans1"
+    }
+]
+
+export{quizDB}
--- script.js
+++ script.js
@@ -1,37 +1,4 @@
-const quizDB = [
-    {
-        question: "Q1:Smaple Queation 1?",
-        a: "option1",
-        b: "option2",
-        c: "option3",
-        d: "option4",
-        ans: "ans4"
-    },
-    {
-        question: "Q1:Smaple Queation 2?",
-        a: "option1",
-        b: "option2",
-        c: "option3",
-        d: "option4",
-        ans: "ans1"
-    },
-    {
-        question: "Q1:Smaple Queation 3?",
-        a: "option1",
-        b: "option2",
-        c: "option3",
-        d: "option4",
-        ans: "ans4"
-    },
-    {
-        question: "Q1:Smaple Queation 4?",
-        a: "option1",
-        b: "option2",
-        c: "option3",
-        d: "option4",
-        ans: "ans1"
-    }
-]
+import { quizDB } from '/DB.js';  
 const question = document.querySelector('.question');
 const option1 = document.querySelector('#option1');
 const option2 = document.querySelector('#option2');
